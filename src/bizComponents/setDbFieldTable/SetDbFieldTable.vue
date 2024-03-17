87834dcd0bcbf2e5a5d99c215975e38ca6fb900b
MonsterXiong <942849672@qq.com>
2024-03-18 01:58:44 +0800

    update

--- src/bizComponents/setDbFieldTable/SetDbFieldTable.vue
+++ src/bizComponents/setDbFieldTable/SetDbFieldTable.vue
@@ -1,46 +1,54 @@
 <template>
   <div class="common-page">
     <el-table :data="tableData" style="width: 100%" height="100%">
-      <el-table-column label="名称" prop="label" ></el-table-column>
-      <el-table-column label="属性" >
+      <el-table-column label="名称" prop="label"></el-table-column>
+      <el-table-column label="属性">
         <template slot-scope="{row}">
           <el-tag size="medium">{{ row.prop }}</el-tag>
         </template>
       </el-table-column>
-      <el-table-column label="类型" align="center" >
+      <el-table-column label="类型" align="center">
         <template slot-scope="{row}">
-          <el-select v-model="row.displayType" placeholder="请选择类型" @change="onChangeDisplayType($event,row)">
+          <el-select v-model="row.displayType" placeholder="请选择类型" @change="onChangeDisplayType($event, row)">
             <el-option v-for="item in displayTypeOption" :key="item.value" :label="item.label" :value="item.value">
             </el-option>
           </el-select>
         </template>
       </el-table-column>
-      <el-table-column label="是否主键" align="center" >
-        <template slot-scope="{ row }">
-          <el-switch v-model="row.isPrimaryKey" active-color="#13ce66" inactive-color="#ff4949"
-            @change="onChangePrimaryKey($event,row)"></el-switch>
-        </template>
-      </el-table-column>
-      <el-table-column label="是否隐藏" align="center" >
+      <template v-if="columnConfig.isPrimaryKey">
+        <el-table-column label="是否主键" align="center">
+          <template slot-scope="{ row }">
+            <el-switch v-model="row.isPrimaryKey" active-color="#13ce66" inactive-color="#ff4949"
+              @change="onChangePrimaryKey($event, row)"></el-switch>
+          </template>
+        </el-table-column>
+      </template>
+      <el-table-column label="是否隐藏" align="center">
         <template slot-scope="{ row }">
           <el-switch v-model="row.isHidden" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
         </template>
       </el-table-column>
-      <el-table-column label="搜索项" align="center" >
-        <template slot-scope="{ row }">
-          <el-switch v-model="row.isQuery" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
-        </template>
-      </el-table-column>
-      <el-table-column label="表单项" align="center" >
-        <template slot-scope="{ row }">
-          <el-switch v-model="row.isForm" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
-        </template>
-      </el-table-column>
-      <el-table-column label="是否必填" align="center" >
-        <template slot-scope="{ row }">
-          <el-switch v-model="row.isRequired" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
-        </template>
-      </el-table-column>
+      <template v-if="columnConfig.isQuery">
+        <el-table-column label="搜索项" align="center">
+          <template slot-scope="{ row }">
+            <el-switch v-model="row.isQuery" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+          </template>
+        </el-table-column>
+      </template>
+      <template v-if="columnConfig.isForm">
+        <el-table-column label="表单项" align="center">
+          <template slot-scope="{ row }">
+            <el-switch v-model="row.isForm" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+          </template>
+        </el-table-column>
+      </template>
+      <template v-if="columnConfig.isForm">
+        <el-table-column label="是否必填" align="center">
+          <template slot-scope="{ row }">
+            <el-switch v-model="row.isRequired" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+          </template>
+        </el-table-column>
+      </template>
       <el-table-column label="操作">
         <template slot-scope="{ row }" v-if="row.displayType == 'select'">
           <el-button size="mini" @click="onConfig(row)">配置</el-button>
@@ -56,72 +64,82 @@ import { EMIT_EVENT_TYPE } from '@/common/constant/emitEventType'
 import { FIELD_DEFAULT_CONFIG } from './config'
 export default {
   props: {
+    columnConfig: {
+      type: Object,
+      default: () => {
+        return {
+          isQuery: true,
+          isForm: true,
+          isPrimaryKey:true
+        }
+      }
+    },
     projectId: {},
     tableName: {},
     isRemote: {
-      type:Boolean,
-      default:true
+      type: Boolean,
+      default: true
     },
-    sourceList:{}
+    sourceList: {}
   },
   data() {
     return {
       sourceTableData: [],
       tableData: [],
-      displayTypeOption:[{
-        value:'input',
-        label:'文本框'
-      },{
-        value:'inputNumber',
-        label:'数字文本框'
-      },{
-        value:'select',
-        label:'下拉框'
-      },{
-        value:'treeSelect',
-        label:'树形下拉框'
-      },{
-        value:'textarea',
-        label:'文本域'
-      },{
-        value:'radio',
-        label:'单选框'
-      },{
-        value:'datetime',
-        label:'日期时间'
-      },{
-        value:'date',
-        label:'日期'
-      },{
-        value:'time',
-        label:'时间'
-      },{
-        value:'checkbox',
-        label:'多选框'
-      },{
-        value:'switch',
-        label:'开关'
-      },{
-        value:'colorPicker',
-        label:'颜色选择器'
-      },{
-        value:'rate',
-        label:'评分'
-      },{
-        value:'slider',
-        label:'滑块'
-      },{
-        value:'image',
-        label:'图片'
+      displayTypeOption: [{
+        value: 'input',
+        label: '文本框'
+      }, {
+        value: 'inputNumber',
+        label: '数字文本框'
+      }, {
+        value: 'select',
+        label: '下拉框'
+      }, {
+        value: 'treeSelect',
+        label: '树形下拉框'
+      }, {
+        value: 'textarea',
+        label: '文本域'
+      }, {
+        value: 'radio',
+        label: '单选框'
+      }, {
+        value: 'datetime',
+        label: '日期时间'
+      }, {
+        value: 'date',
+        label: '日期'
+      }, {
+        value: 'time',
+        label: '时间'
+      }, {
+        value: 'checkbox',
+        label: '多选框'
+      }, {
+        value: 'switch',
+        label: '开关'
+      }, {
+        value: 'colorPicker',
+        label: '颜色选择器'
+      }, {
+        value: 'rate',
+        label: '评分'
+      }, {
+        value: 'slider',
+        label: '滑块'
+      }, {
+        value: 'image',
+        label: '图片'
       }],
-      fieldList:[]
+      fieldList: []
     }
   },
   watch: {
     tableName: {
       async handler(newValue) {
         if (newValue) {
-          if(!this.sourceList?.length){
+          if (!this.sourceList?.length) {
             await this.getFieldList(newValue)
             this.initTemplate(this.fieldList)
           }
@@ -137,26 +155,33 @@ export default {
     },
   },
   methods: {
-    initTemplate(data){
-      this.tableData= data.map(item => {
+    initTemplate(data) {
+      this.tableData = data.map(item => {
         const { Field, Comment, Key, Null } = item
         const isPrimaryKey = Key == 'PRI'
         const isIncludeName = Field?.endsWith('name')
         const isAllowNull = Null == 'YES'
-        return {
+        const initParam = {
           prop: Field,
           label: Comment,
           displayType: 'input',
-          isQuery: isIncludeName,
-          isForm: isIncludeName,
-          isRequired: !isAllowNull,
           isHidden: isPrimaryKey || !isIncludeName,
-          isPrimaryKey,
-          //
-          placeholder:Comment,
+          placeholder: Comment,
           param: {},
           configParam: {}
         }
+        if (this.columnConfig['isQuery']) {
+          initParam.isQuery = isIncludeName
+        }
+        if (this.columnConfig['isForm']) {
+          initParam.isForm = isIncludeName
+          initParam.isRequired = !isAllowNull
+        }
+        // 是否删除
+        if (this.columnConfig['isPrimaryKey']) {
+          initParam.isPrimaryKey = isPrimaryKey
+        }
+        return initParam
       })
     },
     async getFieldList(tableName) {
@@ -168,27 +193,27 @@ export default {
       //   this.initTemplate(data)
       // }
     },
-    onChangeDisplayType(value,row){
-      console.log('value',value,row);
+    onChangeDisplayType(value, row) {
+      console.log('value', value, row);
       const fieldBaseConfig = FIELD_DEFAULT_CONFIG[value] || {}
       let configParam = {}
-      if(value == 'select'){
-         configParam = {
+      if (value == 'select') {
+        configParam = {
           ...fieldBaseConfig,
           // mock enum interface
-          type:"mock",
-          param:{},
-          data:[{value:'monster',label:'大雄'}]
+          type: "mock",
+          param: {},
+          data: [{ value: 'monster', label: '大雄' }]
           // 切换为enum
           // param：{}
         }
       }
-      this.$set(row,'configParam',configParam)
-      console.log('row',row);
+      this.$set(row, 'configParam', configParam)
+      console.log('row', row);
 
 
     },
-    onChangePrimaryKey(value,row) {
+    onChangePrimaryKey(value, row) {
       if (!value) {
         row.isPrimaryKey = true
         return this.$message.warning('必须存在唯一主键')
@@ -204,7 +229,7 @@ export default {
       return this.tableData
     },
     onConfig(row) {
-      this.$emitter.emit(EMIT_EVENT_TYPE.OPEN_FIELD_CONFIG_DIALOG,{projectId:this.projectId,row})
+      this.$emitter.emit(EMIT_EVENT_TYPE.OPEN_FIELD_CONFIG_DIALOG, { projectId: this.projectId, row })
     },
   }
 }
--- src/pages/genSoftware/pageDesign/template/generalTable/GeneralTable.vue
+++ src/pages/genSoftware/pageDesign/template/generalTable/GeneralTable.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="common-page">
+    <!-- 页面配置 -->
+    <!-- 不管如何表格是有的 -->
+    <!-- 是否要分页 -->
+    <!-- 是否有搜索 -->
+    <!-- 是否有弹窗 -->
+    <!-- 是否有编辑 -->
+    <!-- 是否有新增 -->
     <SplitLayout :projectId="projectId" title="通用表格配置">
       <div class="general-table">
-        <SelectDbTable :projectId="projectId" @onChange="onCurrentTableName" ref="selectDbTableRef"></SelectDbTable>
-        <SetDbFieldTable :projectId="projectId" :tableName="tableName" :sourceList="sourceList" :isRemote="isRemote" ref="setDbFieldTableRef"></SetDbFieldTable>
+        <div style="display:flex;gap:10px">
+          <SelectDbTable :projectId="projectId" @onChange="onCurrentTableName" ref="selectDbTableRef"></SelectDbTable>
+          <el-form :inline="true" class="demo-form-inline" style="display: flex;align-items: center;height:100%">
+            <el-form-item label="功能">
+              <el-checkbox-group v-model="configFormData">
+                <el-checkbox label="isQuery">搜索</el-checkbox>
+                <el-checkbox label="isForm">编辑/新增</el-checkbox>
+              </el-checkbox-group>
+            </el-form-item>
+          </el-form>
+        </div>
+        <SetDbFieldTable :columnConfig="columnConfig" :projectId="projectId" :tableName="tableName"
+          :sourceList="sourceList" :isRemote="isRemote" ref="setDbFieldTableRef"></SetDbFieldTable>
       </div>
     </SplitLayout>
   </div>
@@ -15,7 +33,7 @@ import SplitLayout from '@/components/splitLayout/SplitLayout.vue';
 export default {
   props: {
     projectId: {},
-    menuDetailInfo:{}
+    menuDetailInfo: {}
   },
   components: {
     SelectDbTable,
@@ -25,42 +43,63 @@ export default {
   data() {
     return {
       tableName: null,
-      isRemote:false,
-      sourceList:[]
+      isRemote: false,
+      sourceList: [],
+      columnConfig: {
+        isQuery:false,
+        isForm:false,
+        // isPrimaryKey:false
+      },
+      configFormData: []
     }
   },
   watch: {
-    menuDetailInfo:{
+    menuDetailInfo: {
       handler(newValue, oldValue) {
         const menuParam = JSON.parse(newValue.menuParam)
-        const {templateParam} = menuParam
-        if(templateParam?.tableCode){
+        const { templateParam } = menuParam
+        if (templateParam?.tableCode) {
           this.isRemote = false
-          const { tableCode ,attrs} = templateParam
-          this.$nextTick(()=>{
+          const { tableCode, attrs } = templateParam
+          this.$nextTick(() => {
             this.$refs.selectDbTableRef.setCurrentValue(tableCode)
             this.sourceList = attrs
           })
-        }else{
+        } else {
           this.isRemote = true
         }
       },
-      immediate:true
+      immediate: true
+    },
+    configFormData: {
+      handler(newValue) {
+        this.changeFunctionConfig(newValue)
+      },
+      immediate: true
     }
   },
   methods: {
+    changeFunctionConfig(value) {
+      Object.keys(this.columnConfig).forEach(key=>{
+        this.$set(this.columnConfig, key, this.configFormData.includes(key))
+      })
+      this.$set(this.columnConfig, 'isPrimary', value.includes('isForm'))
+    },
     onCurrentTableName(tableName) {
       this.isRemote = true
       this.sourceList = []
       this.tableName = tableName
     },
-    getInfo(){
+    getInfo() {
       const tableInfo = this.$refs.selectDbTableRef.getCurrentNode(this.tableName)
       const fieldInfo = this.$refs.setDbFieldTableRef.getTableData()
       const templateParam = {
-          tableCode: tableInfo.name,
-          tableName: tableInfo.comment || '',
-          attrs: [...fieldInfo]
+        tableCode: tableInfo.name,
+        tableName: tableInfo.comment || '',
+        pageConfig:{
+          ...this.columnConfig
+        },
+        attrs: [...fieldInfo]
       }
       return templateParam
     }
